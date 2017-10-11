9f1aec12b957c74ef7fa40d7f234998ddc99575f
Sam Coward <scoward@pivotal.io>
2017-10-11 16:46:12 -0400

    wip; Adding a lil bit debug
    Signed-off-by: Victoria Henry <vhenry@pivotal.io>

--- containerpath/containerpath_windows2012R2.go
+++ containerpath/containerpath_windows2012R2.go
@@ -3,10 +3,15 @@
 package containerpath
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
 
 func For(path string) string {
+	fmt.Fprintf(os.Stderr, "\nPath is %v", path)
+	fmt.Fprintf(os.Stderr, "\nEnv is %v", os.Getenv("USERPROFILE"))
+	fmt.Fprintf(os.Stderr, "\nCleaned, Env is %v", filepath.Clean(os.Getenv("USERPROFILE")))
+	fmt.Fprintf(os.Stderr, "\nJoined Cleaned, Env is %v", filepath.Join(filepath.Clean(os.Getenv("USERPROFILE")), path))
 	return filepath.Join(filepath.Clean(os.Getenv("USERPROFILE")), path)
 }
